bde1762bf00328fd72356c858dbece5229b39e4e
MyWooWi <1063411064@qq.com>
2017-11-22 09:53:19 +0800

    yes

---
+++ Vue-jing3/js/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '.container',
+    components: {
+        "kcb-header": header,
+        "kcb-swiper": swiper,
+        "kcb-money": money,
+    }
+})
---
+++ vue-router/vue-loader-demo/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>welcome Vue</h1>
+</template>
+<style>
+    
+</style>
+<script>
+    
+</script>
